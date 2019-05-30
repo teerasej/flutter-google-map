99bfefc19e0a98847026b307b0db202eb4d95091
Teerasej <teerasej@gmail.com>
2019-05-30 22:04:23 +0700

    Add init location

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
 
   @override
   Widget build(BuildContext context) {
